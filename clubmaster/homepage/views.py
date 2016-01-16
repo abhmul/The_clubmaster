0da3a5dab4e14659bd9ba4758c6086dac841b332
Avery Jordan <averyjordan1@gmail.com>
2016-01-16 13:44:37 -0600

    Update views.py

--- clubmaster/homepage/views.py
+++ clubmaster/homepage/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 def index(request):
 
 	context = {}
-	return render(request, 'homepage/registration_template.html', context)
+	return render(request, 'homepage/index.html', context)
