686cb5febec6d2c3108320ed9b7a633620180dd9
Spencer Chang <spencerc99@gmail.com>
2016-01-17 01:03:01 -0600

    add member updates

--- clubmaster/attendance/views.py
+++ clubmaster/attendance/views.py
@@ -3,6 +3,8 @@ from .models import Member
 from django.contrib.auth.models import User
 from organizations.models import Organization
 
+from .forms import MemberCreationForm
+
 # Create your views here.
 
 def attendance_view(request):
@@ -14,5 +16,14 @@ def attendance_view(request):
 	for member in member_list:
 		atrr = member.to_list
 		member_attrs.append(attr)
-	context = {}
+	form = MemberCreationForm(request.POST or None)
+	context = {'form':form}
+	if form.is_valid():
+		Member(first_name = form.cleaned_data['first_name']
+	    last_name = form.cleaned_data['last_name']
+	    email = form.cleaned_data['phone_number']
+	    phone_number = form.cleaned_data['phone_number']
+	    college = form.cleaned_data['college']
+	    grad_year = form.cleaned_data['grad_year']).save()
+		
 	return render(request, 'attendance/attendance.html', context)
